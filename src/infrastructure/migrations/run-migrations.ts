4ca0a46844cc53b24ea60e0650cf5899756554a7
gabbrieu <gabrielhmendes@hotmail.com>
2023-11-09 12:34:21 -0300

    info: add console log in migration run script

--- src/infrastructure/migrations/run-migrations.ts
+++ src/infrastructure/migrations/run-migrations.ts
@@ -2,3 +2,5 @@ import { db } from '@infrastructure/config';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 migrate(db, { migrationsFolder: 'drizzle' });
+
+console.info('Migration ran successfully :)');

1abf80d1e457b29955476a353aa355e170fd4297
gabbrieu <gabrielhmendes@hotmail.com>
2023-11-09 16:49:28 -0300

    feat: add Docker to project

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM oven/bun
+
+WORKDIR /app
+
+COPY package.json .
+COPY bun.lockb .
+
+RUN bun install --production
+
+COPY src src
+COPY tsconfig.json .
+COPY drizzle drizzle
+
+ENV NODE_ENV production
+CMD ["bun", "src/index.ts"]
+
+EXPOSE 3000
--- src/infrastructure/config/db.ts
+++ src/infrastructure/config/db.ts
@@ -1,5 +1,5 @@
 import Database from 'bun:sqlite';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 
-const sqliteDatabase: Database = new Database('database.sqlite');
+const sqliteDatabase: Database = new Database('./db/database.sqlite');
 export const db = drizzle(sqliteDatabase);
